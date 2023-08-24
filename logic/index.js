69e1f6ffa886811b215f38128ea751db8a5b7e98
theophilus-mercifield <128309455+theophilus-mercifield@users.noreply.github.com>
2023-08-24 01:31:44 +0000

    rules modal logic done

--- logic/index.js
+++ logic/index.js
@@ -1,16 +1,16 @@
 const ruleBtn = document.getElementById('rules-button');
 const rulesModal = document.getElementById('rules-modal-container');
-const rulesModalBackground = document.getElementById('rules-modal-background');
+const rulesModalBackground = document.getElementById('rules-modal-background')
 const closeBtn = document.getElementById('close-btn');
 
 ruleBtn.addEventListener('click', () => {
     rulesModal.classList.remove('hidden');
     rulesModalBackground.classList.remove('hidden');
-})
+});
 
 closeBtn.addEventListener('click',() => {
     rulesModal.classList.add('hidden');
-    rulesModalBackground.classList.add('hidden');
+    rulesModalBackground.classList.add('hidden');   
 });
 
 rulesModalBackground.addEventListener('click',() => {
