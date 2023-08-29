01e883efb250a48b5036513d4fe94f0bb97d9b7a
Theophilus Mercifield <theophilus.mercifield@amalitech.org>
2023-08-29 18:28:55 +0000

    Merge refix-branch-2 into refix-branch

--- logic/index.js
+++ logic/index.js
@@ -1,19 +1,75 @@
-const ruleBtn = document.getElementById('rules-button');
-const rulesModal = document.getElementById('rules-modal-container');
-const rulesModalBackground = document.getElementById('rules-modal-background');
-const closeBtn = document.getElementById('close-btn');
+const ruleBtn = document.getElementById("rules-button");
+const rulesModal = document.getElementById("rules-modal-container");
+const rulesModalBackground = document.getElementById("rules-modal-background");
+const closeBtn = document.getElementById("close-btn");
 
-ruleBtn.addEventListener('click', () => {
-    rulesModal.classList.remove('hidden');
-    rulesModalBackground.classList.remove('hidden');
-})
+// Step Variables //
+const stepOne = document.getElementById("step-one");
+const stepTwo = document.getElementById("step-two");
+const stepTwoContainer = document.querySelector(".step-two-container");
+const stepThreeContainer = document.querySelector(".step-three");
 
-closeBtn.addEventListener('click',() => {
-    rulesModal.classList.add('hidden');
-    rulesModalBackground.classList.add('hidden');
+//////// Listener on Each Choice ////////
+
+const choiceContainer = document.querySelector(".step-one-container");
+
+// Get User Choice //
+choiceContainer.addEventListener("click", (e) => {
+  if (!e.target.closest("div")) return;
+  const userChoice = e.target.getAttribute("id");
+
+  getComputerChoice(userChoice); 
+  showResults(userChoice, getComputerChoice(userChoice)); 
 });
 
-rulesModalBackground.addEventListener('click',() => {
-    rulesModal.classList.add('hidden');
-    rulesModalBackground.classList.add('hidden');
+// Get Computer Choice //
+function getComputerChoice(userChoice) {
+  const possibleChoices = ["paper", "scissors", "rock"];
+  let remainingChoices = 0;
+
+  if (userChoice === "paper") {
+    remainingChoices = ["scissors", "rock"];
+  } 
+  else if (userChoice === "scissors") {
+    remainingChoices = ["paper", "rock"];
+  }
+  else if (userChoice === "rock") {
+    remainingChoices = ["scissors", "paper"];
+  }
+
+  // Generate a random choice for computer //
+
+  const i = Math.floor(Math.random() * remainingChoices.length);
+  const computerChoice = remainingChoices[i];
+
+  return computerChoice;
+}
+
+
+function showResults(userChoice, computerChoice) {
+  console.log(userChoice);
+  console.log(computerChoice);
+
+  // stepOne.style.display = "none";
+  // stepTwo.style.display = "flex";
+
+  // stepTwoContainer.style.display = "flex";
+  // stepThreeContainer.style.display = "flex";
+}
+
+/////////////////// Modal ///////////////////////
+
+ruleBtn.addEventListener("click", () => {
+  rulesModal.classList.remove("hidden");
+  rulesModalBackground.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+  rulesModal.classList.add("hidden");
+  rulesModalBackground.classList.add("hidden");
+});
+
+rulesModalBackground.addEventListener("click", () => {
+  rulesModal.classList.add("hidden");
+  rulesModalBackground.classList.add("hidden");
 });
